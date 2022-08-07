54b66c28c2e0325512223e563441d907dea80d7a
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-08-07 23:25:09 +0430

    App.py in progress

---
+++ App.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QApplication
+# from MainModel import speak
+from SpeakModel import Speak,SpeakCore,speakObject
+from MainWindow import MainWindow
+
+app = QApplication([])
+
+window = MainWindow(speakObject)
+window.show()
+app.exec()
